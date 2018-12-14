f1a6d3505e02cd509876a792a9facf1dc3b94241
Randy Champagne <champagne.randy@gmail.com>
2018-12-14 14:56:47 -0500

    now with react-testing-library

--- assessment-2/src/App.test.js
+++ assessment-2/src/App.test.js
@@ -1,14 +1,24 @@
 import React from "react";
 import { shallow } from "enzyme";
+import { render } from "react-testing-library";
 import App from "./App";
 
-it("renders without crashing", () => {
-  shallow(<App />);
+describe("jest-enzyme", () => {
+  it("renders without crashing", () => {
+    shallow(<App />);
+  });
+
+  it("renders welcome message", () => {
+    const wrapper = shallow(<App />);
+    const welcome = <h2>Welcome to React</h2>;
+    // expect(wrapper.contains(welcome)).toBe(true);
+    expect(wrapper.contains(welcome)).toEqual(true);
+  });
 });
 
-it("renders welcome message", () => {
-  const wrapper = shallow(<App />);
-  const welcome = <h2>Welcome to React</h2>;
-  // expect(wrapper.contains(welcome)).toBe(true);
-  expect(wrapper.contains(welcome)).toEqual(true);
+describe("react-testing-library", () => {
+  it("renders welcome message", () => {
+    const { getByText } = render(<App />);
+    expect(getByText("Welcome to React")).toBeInTheDocument();
+  });
 });
