d03da32e69a3a29b56103189152e79f9e99ac5d1
Randy Champagne <champagne.randy@gmail.com>
2018-12-16 16:36:29 -0500

    Improved accessibility

--- assessment-1/src/App.js
+++ assessment-1/src/App.js
@@ -1,40 +1,45 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import logo from "./logo.svg";
 import hotelExterior from "./hotelexterior.jpg";
-import "./App.css";
+import "./App.scss";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <nav class="nav" role="navigation">
-          <button id="button__back" class="button nav__putton" type="button">
-            Back
-          </button>
-          <h1 class="nav__header">Hotel Details</h1>
-          <img class="nav__logo" src={logo} alt="logo" />
-        </nav>
-
+      <Fragment>
         <header className="header" role="banner">
-          <hgroup>
+          <nav className="nav" role="navigation">
+            <h1 className="nav__header">Hotel Details</h1>
+            <img className="nav__logo" src={logo} alt="hilto logo" />
+            <button
+              id="button__back"
+              className="button nav__putton"
+              type="button"
+            >
+              Back
+            </button>
+          </nav>
+          <figure role="figure" aria-describedby="hilton-chicago__contact">
             <img
               src={hotelExterior}
               className="header__image"
-              alt="hotel exterior"
+              alt="hilto chicago hotel exterior"
             />
-            <h1>Hilton Chicago</h1>
-          </hgroup>
-          <address className="header__address">
-            720 South Michigan Avenue
-            <br />
-            Chicago, Illinois, 60605
-            <br />
-          </address>
-          <a className="header__tel" href="tel:1-312-922-4400">
-            1-312-922-4400
-          </a>
+            <figcaption id="hilton-chicago__contact">
+              <h1>Hilton Chicago</h1>
+              <address className="header__address">
+                720 South Michigan Avenue
+                <br />
+                Chicago, Illinois, 60605
+                <br />
+              </address>
+              <a className="header__tel" href="tel:1-312-922-4400">
+                1-312-922-4400
+              </a>
+            </figcaption>
+          </figure>
         </header>
-      </div>
+      </Fragment>
     );
   }
 }
