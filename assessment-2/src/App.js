89bc1f64c14785669ab8c2f67c0bcfed5024c954
Randy Champagne <champagne.randy@gmail.com>
2018-12-19 11:13:57 -0500

    Init room block form

--- assessment-2/src/App.js
+++ assessment-2/src/App.js
@@ -1,8 +1,35 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
+import Room from "./Room";
+import rooms from "./room.data";
 import "./App.scss";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      activeRooms: new Map()
+    };
+    this.handleToggleRoomActivation = this.handleToggleRoomActivation.bind(
+      this
+    );
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+  }
+
+  handleToggleRoomActivation(e) {
+    const room = e.target.name;
+    const isActive = e.target.checked;
+    this.setState(prevState => ({
+      activeRooms: prevState.activeRooms.set(room, isActive)
+    }));
+  }
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    console.log("Submitted!");
+    // console.dir(this.refs.name.value);
+  };
+
   render() {
     return (
       <section className="App">
@@ -10,6 +37,24 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1>Hilton Assessment 2</h1>
         </header>
+        <main className="room-block" role="main">
+          <form className="room-block__form" onSubmit={this.handleFormSubmit}>
+            <fieldset className="room-block__form__room--container">
+              {rooms.map(room => (
+                <div key={room.key} className="room-block__form__room--wrapper">
+                  <Room
+                    name={room.name}
+                    isActive={this.state.activeRooms.get(room.name)}
+                    onToggleActivation={this.handleToggleRoomActivation}
+                  />
+                </div>
+              ))}
+            </fieldset>
+            <button className="room-block__form__submit" type="submit">
+              Submit
+            </button>
+          </form>
+        </main>
       </section>
     );
   }
---
+++ assessment-2/src/room.data.js
@@ -0,0 +1,24 @@
+const rooms = [
+  {
+    name: "room-1",
+    key: "room1",
+    label: "Room 1"
+  },
+  {
+    name: "room-2",
+    key: "room2",
+    label: "Room 2"
+  },
+  {
+    name: "room-3",
+    key: "room3",
+    label: "Room 3"
+  },
+  {
+    name: "room-4",
+    key: "room4",
+    label: "Room 4"
+  }
+];
+
+export default rooms;
